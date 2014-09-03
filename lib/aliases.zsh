85614e7e9775b71626847b18a4e196a3e6c6e34b
jasonchi <chijinsong@gmail.com>
2014-09-03 22:59:03 +0800

    change aliases and add template

--- lib/aliases.zsh
+++ lib/aliases.zsh
@@ -33,3 +33,43 @@ alias la='ls -lAh'
 
 alias afind='ack-grep -il'
 
+############################################################
+## Bundler
+############################################################
+
+alias b="bundle"
+alias bi="b install"
+alias bu="b update"
+alias be="b exec"
+
+############################################################
+## Rails
+############################################################
+
+alias rs="rails server"
+alias rg="rails generate"
+alias rc="rails console"
+alias tl='tail -f log/development.log'
+
+############################################################
+## Vulcan
+############################################################
+alias cdv='cd ~/work/vulcan/'
+alias cdva='cdv ; cd vulcan-affiliate'
+alias cdvb='cdv ; cd vulcan-bing'
+alias cdvc='cdv ; cd vulcan-common'
+alias cdvd='cdv ; cd vulcan-data-interface'
+alias cdvg='cdv ; cd vulcan-google'
+alias cdvn='cdv ; cd vulcan-benchmark'
+alias cdvr='cdv ; cd reporting'
+alias cdvs='cdv ; cd sales-master'
+alias cdvt='cdv ; cd tracking'
+alias cdvp='cdv ; cd transporter'
+
+alias irbv="irb -r ~/work/vulcan/irb_vulcan.rb"
+alias pryv="bundle exec pry -r ~/projects/vulcan/irb_vulcan.rb"
+
+alias tst='VULCAN_ENV=test RAILS_ENV=test'
+alias prod='VULCAN_ENV=production RAILS_ENV=production'
+
+alias v='ruby bin/vulcan'
